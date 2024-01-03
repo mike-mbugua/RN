19eb618a108bbeab9b4fcc02f453dda4725ac2d3
michael mbugua <mikembugua960@gmail.com>
2024-01-03 21:09:38 +0300

    add a pressable feature

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,9 +1,14 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Pressable } from "react-native";
 
 function GoalItem(props) {
   return (
-    <View style={styles.goals} key={Math.random()}>
-      <Text style={styles.goalText}>{props.text}</Text>
+    <View style={styles.goals}>
+      <Pressable
+        onPress={props.addOnDeleteHandler.bind(this, props.id)}
+        android_ripple={{ color: "#210664" }}
+      >
+        <Text style={styles.goalText}>{props.text}</Text>
+      </Pressable>
     </View>
   );
 }
@@ -12,12 +17,13 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goals: {
     borderWidth: 1,
-    padding: 10,
+    // padding: 0,
     margin: 8,
     borderRadius: 6,
     backgroundColor: "blue",
   },
   goalText: {
     color: "#cccccc",
+    padding: 10,
   },
 });
