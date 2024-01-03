0c69510d6646bb6324513d583ded090381123e18
michael mbugua <mikembugua960@gmail.com>
2024-01-03 21:08:51 +0300

    seting the modal

--- App.js
+++ App.js
@@ -1,23 +1,48 @@
 import { useState } from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { Button, FlatList, StyleSheet, Text, View } from "react-native";
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
 
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
+  function startAddGoalHandler() {
+    setModalIsVisible(true);
+  }
 
   function addGoalHandler(enteredText) {
-    setCourseGoals((currentGoals) => [...currentGoals, enteredText]);
+    if (enteredText.trim() !== "") {
+      setCourseGoals((currentGoals) => [
+        ...currentGoals,
+        { text: enteredText, id: Math.random().toString() },
+      ]);
+    }
+    alert("cannot add empty goals");
+  }
+
+  function onDeleteHandler(id) {
+    setCourseGoals((currentCourseGoals) => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    });
   }
   return (
     <View style={styles.appContainer}>
-      <GoalInput onAddGoal={addGoalHandler} />
-      <View style={styles.goalContainer}>
-        <Text>list of goals ....</Text>
+      <Button title="Add new Goal" onPress={startAddGoalHandler} />
 
+      {modalIsVisible && (
+        <GoalInput visible={modalIsVisible} onAddGoal={addGoalHandler} />
+      )}
+      <View style={styles.goalContainer}>
         <FlatList
           data={courseGoals}
-          renderItem={(itemData) => <GoalItem text={itemData.item} />}
+          renderItem={(itemData) => (
+            <GoalItem
+              text={itemData.item.text}
+              addOnDeleteHandler={onDeleteHandler}
+              id={itemData.item.id}
+            />
+          )}
         />
       </View>
     </View>
